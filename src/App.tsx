a45e6deb6f97fbd3d3a860f8439f6406893b0d6d
unknown <>
2021-07-15 12:44:36 -0400

    Added bootstrap and navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,19 @@
 import React from "react";
 import weatherApiClient from "./api/weather-api-client";
 import "./App.css";
+import NavBar from "./components/nav/NavBar";
 
 function App() {
   const click = (e: any) => {
     e.preventDefault();
     weatherApiClient.byCityName({ q: ["London", "uk"] }).then((data) => {
-      console.log(data.main);
+      console.log(data);
     });
   };
 
   return (
     <div className="App">
+      <NavBar />
       <p>Hello World</p>
       <button onClick={click}>test</button>
     </div>
