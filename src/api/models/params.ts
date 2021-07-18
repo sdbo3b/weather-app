6a2ceef331e2e8a0d1c1454b9a4423310d8a7918
unknown <>
2021-07-18 10:50:39 -0400

    Added docs for params

--- src/api/models/params.ts
+++ src/api/models/params.ts
@@ -1,6 +1,11 @@
 import { Mode, Units, Lang } from "./index";
 
 export interface ByCityNameParams {
+  /**[0] = City Name
+   *
+   * [1] = State Code (If US city)
+   *
+   * [2] = Country Code */
   q: [string, string?, string?];
   mode?: Mode;
   units?: Units;
@@ -30,6 +35,17 @@ export interface ByZipCodeParams {
 }
 
 export interface ByRectangleZoneParams {
+  /** Bounding box:
+   *
+   * [0] = lon-left
+   *
+   * [1] = lat-bottom
+   *
+   * [2] = lon-right
+   *
+   * [3] = lat-top
+   *
+   * [4] = zoom */
   bbox: [number, number, number, number, number];
   units?: Units;
   lang?: Lang;
